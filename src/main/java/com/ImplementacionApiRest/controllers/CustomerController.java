3b941d34b7a4ed35e99038b7802e66c2e9b64ccc
Cecilia <mirascecilia08@gmail.com>
2025-10-20 20:24:26 -0300

    octavo commit imprementacion de la clase ResponseEntity

--- src/main/java/com/ImplementacionApiRest/controllers/CustomerController.java
+++ src/main/java/com/ImplementacionApiRest/controllers/CustomerController.java
@@ -1,6 +1,8 @@
 package com.ImplementacionApiRest.controllers;
 
 import com.ImplementacionApiRest.domian.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -18,10 +20,10 @@ public class CustomerController {
     @GetMapping("/clientes")
     //Forma de hacerlo a nivel método
     //@RequestMapping(method = RequestMethod.GET)
-    public List<Customer>getCustomers()
+    public ResponseEntity<List<Customer>>getCustomers()
     {
 
-        return customers;
+        return ResponseEntity.ok(customers);
     }
 
 
@@ -29,58 +31,58 @@ public class CustomerController {
     @GetMapping("/clientes/{username}")
     //Forma de hacerlo a nivel método
     //@RequestMapping(value = "/{username}",method = RequestMethod.GET)
-    public Customer getCliente(@PathVariable String username){
+    public ResponseEntity<?> getCliente(@PathVariable String username){
         for(Customer c:customers){
             if(c.getUsername().equalsIgnoreCase(username)){
-                return c;
+                return ResponseEntity.ok(c);
             }
 
         }
-        return null;
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente no encontrado con el username: "+username);
 
     }
     //Agregar un cliente a una lista o una base de datos
     @PostMapping("/clientes")
     //Forma de hacerlo a nivel método
    // @RequestMapping(method = RequestMethod.POST)
-    public Customer postCliente(@RequestBody  Customer customer){
+    public ResponseEntity<?> postCliente(@RequestBody  Customer customer){
         customers.add(customer);
-        return customer;
+        return ResponseEntity.status(HttpStatus.CREATED).body("Cliente creado correctamente:"+customer.getUsername());
     }
     //Modificar un cliente
     @PutMapping("/clientes/{username}")
     //Forma de hacerlo a nivel método
     //@RequestMapping(value = "/{username}",method = RequestMethod.PUT)
-    public Customer putCliente(@PathVariable String username,@RequestBody Customer customer){
+    public ResponseEntity<?> putCliente(@PathVariable String username,@RequestBody Customer customer){
         for(Customer c: customers){
           if(c.getId()==customer.getId()){
               c.setName(customer.getName());
               c.setUsername(customer.getUsername());
               c.setPassword(c.getPassword());
-              return c;
+              return ResponseEntity.ok("Cliente modificado correctamente:"+customer.getUsername());
           }
         }
-        return null;
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente no encontrado: "+customer.getUsername());
     }
     //Eliminar un cliente
     @DeleteMapping("/clientes/{id}")
     //Forma de hacerlo a nivel método
     //@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public Customer deleteCliente(@PathVariable int id){
+    public ResponseEntity<?> deleteCliente(@PathVariable int id){
         for(Customer c : customers){
             if(c.getId()==id){
                 customers.remove(c);
-                return c;
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Cliente eliminado satisfactoriamente:"+id);
             }
         }
-        return null;
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente no encontrado con el id: "+id);
     }
 
     //Modificaciones específicas
     @PatchMapping("/clientes")
     //Forma de hacerlo a nivel método
     //@RequestMapping(method = RequestMethod.PATCH)
-    public Customer patchCliente(@RequestBody Customer customer){
+    public ResponseEntity<?> patchCliente(@RequestBody Customer customer){
         for(Customer c : customers){
             if(c.getId()==customer.getId()){
                 if(customer.getName()!=null){
@@ -92,12 +94,12 @@ public class CustomerController {
                 if(customer.getPassword()!=null){
                     c.setPassword(customer.getPassword());
                 }
-                return c;
+                return ResponseEntity.ok("Cliente modificado correctamente: "+customer.getId());
             }
 
 
         }
-      return null;
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente no encontrado con el id: "+customer.getId());
     }
 
 }
