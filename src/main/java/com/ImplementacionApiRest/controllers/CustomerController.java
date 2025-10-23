03cc2861118a4a0f98c0bd00524f33d2ed026857
Cecilia <mirascecilia08@gmail.com>
2025-10-23 20:07:13 -0300

    decimo commit consumo de recursos eficientes modificacion metodo post

--- src/main/java/com/ImplementacionApiRest/controllers/CustomerController.java
+++ src/main/java/com/ImplementacionApiRest/controllers/CustomerController.java
@@ -4,7 +4,9 @@ import com.ImplementacionApiRest.domian.Customer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -45,10 +47,16 @@ public class CustomerController {
     @PostMapping("/clientes")
     //Forma de hacerlo a nivel método
    // @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<?> postCliente(@RequestBody  Customer customer){
+    public ResponseEntity<?> postCliente(@RequestBody  Customer customer) {
         customers.add(customer);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Cliente creado correctamente:"+customer.getUsername());
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{username}")
+                .buildAndExpand(customer.getUsername())
+                .toUri();
+        //return ResponseEntity.created(location).build();
+        return ResponseEntity.created(location).body(customer);
     }
+
     //Modificar un cliente
     @PutMapping("/clientes/{username}")
     //Forma de hacerlo a nivel método
